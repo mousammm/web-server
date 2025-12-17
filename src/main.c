62dcad2321d14e6d25a2cf6055d1e9462ec02d96
mousammm <mxmssdia@gmail.com>
2025-12-17 14:21:35 +0530

    A1: server hello world

--- src/main.c
+++ src/main.c
@@ -1,18 +1,35 @@
-#include "Server.h"
 #include <stdio.h>
+#include <sys/socket.h>     // socket
+#include <netinet/in.h>     // sockaddr
+#include <unistd.h>         // close
+#include <string.h>         // strlen
+
+#define PORT 8080
 		    
 int main() {
-    
-    Server* server = server_create(8000);
-	Client* client = server_accept_client(server); // uses is_runnign accept connection 
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-	// log client ip timestamp time 
-	// read http headers in buffer | and log them like GET PATH VERSION CONTENT TYPE LENGTH
-	// route handling
-	client_handle_request(client);
+    struct sockaddr_in address;
+	address.sin_family = AF_INET;
+	address.sin_port = htons(PORT);
+	address.sin_addr.s_addr = INADDR_ANY;
 
-    close(client->fd);
-    close(server->fd);
-	//server_stop(server); // set is running = 0 
+	bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+	listen(server_fd, 10);
+
+    int client_fd = accept(server_fd, NULL, NULL); // null: no need client info
+
+    char *res = 
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/plain\r\n"
+        "Content-Length: 13\r\n"
+        "\r\n"
+        "Hello, World!";
+
+    send(client_fd, res, strlen(res), 0);
+
+    close(client_fd);
+    close(server_fd);
+
     return 0;
 }
