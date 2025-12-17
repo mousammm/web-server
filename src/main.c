23216e8b90258d46466a0ddd3573f499aa6d7b1a
mousammm <mxmssdia@gmail.com>
2025-12-17 15:00:35 +0530

    A1: client info added

--- src/main.c
+++ src/main.c
@@ -3,13 +3,15 @@
 #include <netinet/in.h>     // sockaddr
 #include <unistd.h>         // close
 #include <string.h>         // strlen
+#include <arpa/inet.h>      // inet_ntop
 
 #define PORT 8080
+
 		    
 int main() {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    struct sockaddr_in address;
+    struct sockaddr_in address, client_address;
 	address.sin_family = AF_INET;
 	address.sin_port = htons(PORT);
 	address.sin_addr.s_addr = INADDR_ANY;
@@ -19,9 +21,18 @@ int main() {
 
     printf("Server running on port %d...\n", PORT);
 
+
     while(1)
     {
-        int client_fd = accept(server_fd, NULL, NULL); // null: no need client info
+	    socklen_t client_addr_length = sizeof(client_address);                      // address length of client 
+	    int client_fd = accept(server_fd, (struct sockaddr*)&client_address, &client_addr_length);
+
+        // Get client info
+        char client_ip[INET_ADDRSTRLEN];
+	    inet_ntop(AF_INET, &client_address.sin_addr, client_ip, sizeof(client_ip));      // get client ip
+        int client_port = ntohs(client_address.sin_port);	                            // get client port 
+        printf("client: %s:%d\n", client_ip, client_port);
+
         char *res = 
             "HTTP/1.1 200 OK\r\n"
             "Content-Type: text/plain\r\n"
@@ -31,7 +42,7 @@ int main() {
 
         send(client_fd, res, strlen(res), 0);
         close(client_fd);
-    }
+    };
 
     close(server_fd);
 
